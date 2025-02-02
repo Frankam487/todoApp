2c0e55789ad5cd756019490fb79830b4778e7c2b
Frankam487 <fkamgang06@gmail.com>
2025-02-03 00:07:02 +0200

    ok

---
+++ app.js
@@ -0,0 +1,38 @@
+const valeurInput = document.getElementById("value");
+const bouton = document.querySelector("button");
+const resultat = document.querySelector("ul");
+
+bouton.addEventListener('click', () => {
+  const valeurFinale = valeurInput.value;
+  
+  if (valeurFinale.trim() !== "") {
+    const nouvelleTache = document.createElement('li');
+    resultat.appendChild(nouvelleTache);
+    nouvelleTache.textContent = valeurFinale;
+    
+  
+    const boutonSuppression = document.createElement('button');
+    boutonSuppression.textContent = 'Effacer❌';
+    nouvelleTache.appendChild(boutonSuppression);
+
+    boutonSuppression.addEventListener('click', () => {
+      nouvelleTache.remove();
+    });
+
+
+    const boutonAccomplir = document.createElement('button');
+    boutonAccomplir.textContent = '✔️';
+    nouvelleTache.appendChild(boutonAccomplir);
+
+    boutonAccomplir.addEventListener('click', () => {
+
+      
+      nouvelleTache.style.textDecoration = 'line-through';
+      nouvelleTache.style.color = 'gray';
+      boutonAccomplir.disabled = true;  
+      boutonAccomplir.textContent = 'Accomplie'; 
+    });
+
+    valeurInput.value = ""; 
+  }
+});
